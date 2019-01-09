2923eef8549e9da47e3939dba1347e0e0d15e779
yuchenli <u6013787@anu.edu.au>
2019-01-10 10:54:58 +1100

    finish output to csv

--- tocsv.py
+++ tocsv.py
@@ -2,7 +2,6 @@
 Yuchen Li
 u6013787@anu.edu.au
 """
-
 import read_data1
 import pandas as pd
 
@@ -10,6 +9,22 @@ array = read_data1.read_all_flat("/home/yl2404/alignmentStatFiles/")
 
 df = pd.DataFrame(array)
 
-df.iloc[1]
+df["experiment_date"] = df["experiment_date"].apply(pd.to_datetime)
 
+machine_name = df[df["machine_name"].notnull()]
+ip_address = df[df["machine_name"].isnull()]
 
+names = machine_name["machine_name"].unique()
+for name in names:
+    tmpdf = machine_name[machine_name["machine_name"] == name]
+
+    tmpdf.sort_values(by="experiment_date").to_csv(name + ".csv")
+
+names = ip_address["host_ip_address"].unique()
+for name in names:
+    if pd.isnull(name):
+        tmpdf = ip_address[ip_address["host_ip_address"].isnull()]
+        tmpdf.sort_values(by="experiment_date").to_csv("NAN.csv")
+    else:
+        tmpdf = ip_address[ip_address["host_ip_address"] == name]
+        tmpdf.sort_values(by="experiment_date").to_csv(name + ".csv")
