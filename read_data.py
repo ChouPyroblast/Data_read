24ce1a46d7570feaecfcbf8395c2cb232a380cc2
yuchenli <u6013787@anu.edu.au>
2019-01-11 16:02:09 +1100

    change files name

--- read_data.py
+++ read_data.py
@@ -2,50 +2,161 @@
 Yuchen Li
 u6013787@anu.edu.au
 
+
 """
 import os
-import json
+import pandas
 
-class Alignment:
-    def __str__(self):
-        return str(vars(self))
+EXPT = "expt.in"  # name of expt file
+ALIGNMENTPARMVALS = "AlignmentParmVals.dat" # name of AF file
 
 
-EXPT = "expt.in"
-ALIGNMENTPARMVALS = "AlignmentParmVals.dat"
+def is_float(s):
+    """
+    :param input a string, if it can be return to float then return float, otherwise return itself:
+    :return: float or string
 
-def is_float(str):
+    str -> float or str
+    """
     try:
-        float(str)
-        return str
+        float(s)
+        return float(s)
+
     except ValueError:
-        return "\""+str+"\""
+        return s
+
 
 def read_file(path):
+
     """
     :param path: the path of file AlignmentParmVals.dat and expt.in
-    :return: instance of Alignment and Expt
+    :return: dictionary of AF and Expt
 
     path -> (Alignment,Expt)
     """
-    expt_file = os.path.join(path,EXPT)
-    alignmentparmvals_file = os.path.join(path,ALIGNMENTPARMVALS)
+
+    expt_file = os.path.join(path, EXPT)
+    alignmentparmvals_file = os.path.join(path, ALIGNMENTPARMVALS)
 
     alignment = None
 
-    if os.path.isfile(alignmentparmvals_file):
-        alignment = Alignment()
-        with open(alignmentparmvals_file,"r") as f:
-            print(alignmentparmvals_file)
+    # read AF
+    if os.path.isfile(alignmentparmvals_file):  # if file exists
+        alignment = {}
+        with open(alignmentparmvals_file, "r") as f:
             for line in f:
-                words = line.replace("\t", " ").split(": ")
-                print(words)
+                words = line.replace("\t", " ").split(": ")  # split the key and value
+
                 name = words[0]
-
                 value = is_float(words[1][:-1])
-                exec("alignment."+name+"="+value)
+                alignment[name] = value
 
-    return alignment
+    # read Expt
+
+    expt = None
+
+    if os.path.isfile(expt_file):
+        stack = []  # to record the bracket
+        expt = {}
+        stack.append(expt)
+
+        with open(expt_file, "r", errors='replace') as f:
+            multi_string = False
+            name = None
+            for line in f:
+                if line == "\n":
+                    continue
+                if "BeginSection" in line:
+                    name = line.split()[1]
+                    new_dic = {}
+                    stack[-1][name] = new_dic
+                    stack.append(new_dic)
+                    continue
+                if "EndSection" in line:
+                    stack.pop()
+                    continue
+                if "__start_multi_string__" in line:
+                    multi_string = True
+                    name = line.split()[0]
+                    stack[-1][name] = ""
+                    continue
+                if "__end_multi_string__" in line:
+                    multi_string = False
+                    continue
+                if multi_string:
+                    stack[-1][name] += line
+                    continue
+
+                words = line.replace("\t", " ").split()
+
+                if len(words) == 1:
+                    name = words[0]
+                    stack[-1][name] = None
+                else:
+                    name = words[0]
+                    value = is_float(" ".join(words[1:]))
+                    stack[-1][name] = value
+
+    return alignment, expt
+
+
+def read_file_flat(path):
+
+    """
+    :param path: the path of file AlignmentParmVals.dat and expt.in
+    :return: dictionary of AF and Expt. Put them together
+
+    path -> dict
+    """
+
+    expt_file = os.path.join(path, EXPT)
+    alignmentparmvals_file = os.path.join(path, ALIGNMENTPARMVALS)
+
+    df = {}
+
+    # read AF
+    if os.path.isfile(alignmentparmvals_file) and os.path.isfile(expt_file):  # if file exists
+        with open(alignmentparmvals_file, "r") as f:
+            for line in f:
+                words = line.replace("\t", " ").split(": ")  # split the key and value
+                name = "AF."+words[0]
+                value = is_float(words[1][:-1])
+                df[name] = value
+
+        # read Expt
+
+        with open(expt_file, "r", errors='replace') as f:
+            multi_string = False
+            name = None
+            for line in f:
+                if line == "\n":
+                    continue
+                if "BeginSection" in line:
+                    continue
+                if "EndSection" in line:
+                    continue
+                if "__start_multi_string__" in line:
+                    multi_string = True
+                    name = line.split()[0]
+                    df[name] = ""
+                    continue
+                if "__end_multi_string__" in line:
+                    multi_string = False
+                    continue
+                if multi_string:
+                    df[name] += line
+                    continue
+
+                words = line.replace("\t", " ").split()
+
+                if len(words) == 1:
+                    continue
+                name = words[0]
+                value = is_float(" ".join(words[1:]))
+                df[name] = value
+    return df
+
+
 
 def read_dir(dir):
     """
@@ -56,15 +167,49 @@ def read_dir(dir):
     """
     array = []
     for path in os.listdir(dir):
-        array.append(read_file(os.path.join(dir,path)))
+        array.append(read_file(os.path.join(dir, path)))
     return array
+
+
+def read_dir_flat(dir):
+    """
+    :param dir:
+    :return: array of instance of Alignment and Expt
+
+    This funciton is used to produce .csv file
+    str -> [dict]
+    """
+    array = []
+    for path in os.listdir(dir):
+        dic = read_file_flat(os.path.join(dir, path))
+        if len(dic) > 0:
+            array.append(dic)
+    return array
+
+
 def read_all(dir):
     for path in os.listdir(dir):
+        yield read_dir(os.path.join(dir, path))
 
-        yield read_dir(os.path.join(dir,path))
 
-if __name__ == "__main__" :
-    ge = read_all("/home/yl2404/alignmentStatFiles/")
-    for items in ge:
-        for item in items:
-            print(item)
+def read_all_flat(dir):
+    """
+    :param dir:
+    :return: array of all data
+    This funciton is used to produce .csv file
+    """
+    array = []
+    for path in os.listdir(dir):
+        array.extend(read_dir_flat(os.path.join(dir, path)))
+    return array
+
+
+if __name__ == "__main__":
+    #item = read_file_flat("/home/yl2404/alignmentStatFiles/ANU3_Testing/5mm_Berea_Standard")
+    #for i in item:
+        #print(i,item[i])
+
+    #print(len(item))
+    item = read_all_flat("/home/yl2404/alignmentStatFiles/")
+    #for i in item:
+        #print(i)
--- tocsv.py
+++ tocsv.py
@@ -1,7 +1,8 @@
-"""
-Yuchen Li
-u6013787@anu.edu.au
-"""
+
+# coding: utf-8
+
+# In[10]:
+
 import read_data1
 import pandas as pd
 
@@ -9,16 +10,39 @@ array = read_data1.read_all_flat("/home/yl2404/alignmentStatFiles/")
 
 df = pd.DataFrame(array)
 
-df["experiment_date"] = df["experiment_date"].apply(pd.to_datetime)
+df["experiment_date"]=df["experiment_date"].apply(pd.to_datetime)
+
 
 machine_name = df[df["machine_name"].notnull()]
 ip_address = df[df["machine_name"].isnull()]
 
+
 names = machine_name["machine_name"].unique()
 for name in names:
-    tmpdf = machine_name[machine_name["machine_name"] == name]
+    tmpdf = machine_name[machine_name["machine_name"]==name]
+    
+    tmpdf.sort_values(by="experiment_date").to_csv(name+".csv")
+    
+names = ip_address["host_ip_address"].unique()
 
-    tmpdf.sort_values(by="experiment_date").to_csv(name + ".csv")
+for name in names:
+    if pd.isnull(name):
+        tmpdf = ip_address[ip_address["host_ip_address"].isnull()]
+        tmpdf.sort_values(by="experiment_date").to_csv("NAN.csv")
+    else:
+        tmpdf = ip_address[ip_address["host_ip_address"]==name]
+        tmpdf.sort_values(by="experiment_date").to_csv(name +".csv")
+
+
+# In[11]:
+
+names = machine_name["machine_name"].unique()
+for name in names:
+    tmpdf = machine_name[machine_name["machine_name"]==name]
+    tmpdf.sort_values(by="experiment_date").to_csv(name+".csv")
+
+
+# In[12]:
 
 names = ip_address["host_ip_address"].unique()
 for name in names:
@@ -26,5 +50,67 @@ for name in names:
         tmpdf = ip_address[ip_address["host_ip_address"].isnull()]
         tmpdf.sort_values(by="experiment_date").to_csv("NAN.csv")
     else:
-        tmpdf = ip_address[ip_address["host_ip_address"] == name]
-        tmpdf.sort_values(by="experiment_date").to_csv(name + ".csv")
+        tmpdf = ip_address[ip_address["host_ip_address"]==name]
+        tmpdf.sort_values(by="experiment_date").to_csv(name +".csv")
+
+
+# In[13]:
+
+metadata = ["project_name","sample_name","machine_name","host_ip_address","experiment_date"]
+EXPT_parameter = ["camera_length","specimen_distance","rotation_x","Vert.detector","phi","camera_theta","camera_psi","voxel_size","pixel_size_x"]
+AF_parameter = ["AF.camera_length","AF.specimen_distance","AF.origin_offset_horizontal","AF.origin_offset_vertical","AF.detector_tilt_phi","AF.detector_tilt_theta","AF.detector_tilt_psi"]
+Diff_parameter = ["Camera distance","Sample distance","Hor. detector","Vert.detector","Phi detector","Theta detector","Psi detector"]
+
+
+# In[14]:
+
+columns = metadata + EXPT_parameter + AF_parameter
+
+
+# In[16]:
+
+#df["experiment_date"]=df["experiment_date"].apply(pd.to_datetime)
+
+df['Vert.detector']= 0
+df['phi']= 0
+
+
+
+selected_df = df[columns]
+
+sorted_columns = metadata.copy()
+for i in range(len(Diff_parameter)):
+    name = "DIFF-"+ Diff_parameter[i]
+    selected_df[name] = selected_df[EXPT_parameter[i]]-selected_df[AF_parameter[i]]
+    sorted_columns.append(EXPT_parameter[i])
+    sorted_columns.append(AF_parameter[i])
+    sorted_columns.append(name)
+    
+    
+
+selected_df["AF.vxsize"] = selected_df["pixel_size_x"] * selected_df["AF.specimen_distance" ]/ selected_df["AF.camera_length"]
+selected_df["vxsize error"] =  1 - selected_df["AF.vxsize"] / selected_df["voxel_size"]
+sorted_columns.append("voxel_size")
+sorted_columns.append("AF.vxsize")
+sorted_columns.append("vxsize error")
+
+selected_df = selected_df[sorted_columns]
+
+machine_name = selected_df[selected_df["machine_name"].notnull()]
+ip_address = selected_df[selected_df["machine_name"].isnull()]
+
+names = machine_name["machine_name"].unique()
+for name in names:
+    tmpdf = machine_name[machine_name["machine_name"]==name]
+    
+    tmpdf.sort_values(by="experiment_date").to_csv(name+".csv")
+    
+names = ip_address["host_ip_address"].unique()
+for name in names:
+    if pd.isnull(name):
+        tmpdf = ip_address[ip_address["host_ip_address"].isnull()]
+        tmpdf.sort_values(by="experiment_date").to_csv("NAN.csv")
+    else:
+        tmpdf = ip_address[ip_address["host_ip_address"]==name]
+        tmpdf.sort_values(by="experiment_date").to_csv(name +".csv")
+
