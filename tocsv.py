29dde62f32203772fccc0cd75cab776644829fcf
yuchenli <u6013787@anu.edu.au>
2019-01-11 16:03:27 +1100

    change import names

--- tocsv.py
+++ tocsv.py
@@ -6,7 +6,7 @@
 import read_data
 import pandas as pd
 
-array = read_data1.read_all_flat("/home/yl2404/alignmentStatFiles/")
+array = read_data.read_all_flat("/home/yl2404/alignmentStatFiles/")
 
 df = pd.DataFrame(array)
 
